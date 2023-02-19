098d5b0e44c84709c2ce17422e193b991735945f
Knut Utne Hollund <kuho@equinor.com>
2023-02-19 23:58:39 +0100

    documenting

--- notebooks/sharp_angle.py
+++ notebooks/sharp_angle.py
@@ -12,29 +12,67 @@ sharpening_setup = {
 
 
 # %%
-class SharpeningAngle:
-    """Documentation for SharpeningAngle"""
-
-    def __init__(self, sharpening_setup):
-        self.radius_wheel = 0.5 * sharpening_setup["Wheel diameter"]
-        self.jig_to_outside_support = sharpening_setup["Jig length"]
-        self.thickness_jig = sharpening_setup["Jig thickness"]
-        self.apex_offset = sharpening_setup["Apex offset"]
-        self.radius_support = 0.5 * sharpening_setup["Support diameter"]
+class AngleCalculator:
+    """
+    Implementation of calculations usefull for grinding
+    
+    """
+    def __init__(
+        """
+        
+        """
+        self, wheel_diameter, support_diameter, jig_lenght, jig_thickness, apex_offset=0
+    ):
+        self.radius_wheel = 0.5 * wheel_diameter
+        self.radius_support = 0.5 * support_diameter
+        self.apex_to_jig_reference = jig_length - self.radius_support
+        self.thickness_jig = jig_thickness
+        self.apex_offset = apex_offset
 
         self.center_support_to_jig_reference = (
             self.radius_support + 0.5 * self.thickness_jig + self.apex_offset
         )
-        self.apex_to_jig_reference = self.jig_to_outside_support - self.radius_support
+
+        # the correction angle due to thickness of jig and apex offset
         self.correction_angle = np.arctan(
             self.center_support_to_jig_reference / self.apex_to_jig_reference
         )
 
+        # pythagoras to calculate
         self.apex_to_center_support = np.sqrt(
             self.center_support_to_jig_reference**2 + self.apex_to_jig_reference**2
         )
 
     def distance_center_axel_to_center_support(self, angle_degree):
+        """
+        For given grinding angle in degrees return the distance from the 
+        center of the axel of the grinding wheel to the center of the support.
+        
+        The Law of cosines (https://en.wikipedia.org/wiki/Law_of_cosines)is used.
+        The vertices of triangle considered are,
+        
+          A: center of axel
+          B: apex of the blade
+          C: center of the support
+          
+        To find the length of BC and angle between AB and BC consider the triangle
+        BCD where the vertices are,
+        
+          B and C as above
+          D: jig reference point
+          
+        The length of BD is the jig_length minus the radius of the support, the 
+        length of CD is the radius of the support plus half the thickness of the 
+        jig plus any apex offset. The angle between BC and CD is 90 degrees and 
+        the lenght of BC can be found by the Pytagorean theorem. This angle,
+        between BD and BC (correction angle) can be found by the inverse tangence 
+        of CD/BD.
+        
+        The grinding angle is the angle between a tangent to the grinding wheel
+        in apex point (B) which is 90 degrees to AB hence the angle between AB and
+        BC is 90 degrees + the grinding angle minus the correction angle.
+        
+        """
         corrected_angle = np.radians(angle_degree) + 0.5 * np.pi - self.correction_angle
         return np.sqrt(
             self.apex_to_center_support**2
@@ -43,6 +81,24 @@ class SharpeningAngle:
             * self.apex_to_center_support
             * self.radius_wheel
             * np.cos(corrected_angle)
+        )
+    
+    def sharpening_angle_degrees_from_radial_distance(self, distance):
+        """
+        For given distance from center of axel to center of support return 
+        the grinding angle in degrees.
+        
+        see documentation of the function distance_center_axel_to_center_support()
+        """
+        corrected_angle = np.acos(
+            (distance**2 + self.apex_to_center_support**2 + self.radius_wheel**2)
+            / (2 * self.apex_to_center_support * self.radius_wheel)
+        )
+        return np.degrees(corrected_angle + self.correction_angle)
+    
+    def sharpening_angle_degrees(self, horisontal, vertical):
+        return self.sharpening_angle_degrees_from_radial_distance(
+            np.sqrt(horisontal**2 + vertical**2)
         )
 
     def distance_outside_support_to_wheel(self, angle_degree):
@@ -68,12 +124,6 @@ class SharpeningAngle:
             - vertical_offset_support**2
         )
 
-    def sharpening_angle_degrees(self, distance):
-        corrected_angle = np.acos(
-            (distance**2 + self.apex_to_center_support**2 + self.radius_wheel**2)
-            / (2 * self.apex_to_center_support * self.radius_wheel)
-        )
-        return np.degrees(corrected_angle + self.correction_angle)
 
 
 # %%
